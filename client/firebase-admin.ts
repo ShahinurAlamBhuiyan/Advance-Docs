7c18b4fcd104cb67e931c864a1807702563b6c9d
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-11-24 19:24:03 +0600

    bug fixed

--- client/firebase-admin.ts
+++ client/firebase-admin.ts
@@ -1,11 +1,12 @@
 import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
+// Load service account credentials from environment variables
 const serviceKey = {
   type: process.env.TYPE,
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
+  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"), // Handle newline in private key
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
@@ -17,6 +18,7 @@ const serviceKey = {
 
 let app: App;
 
+// Initialize Firebase Admin SDK
 if (getApps().length === 0) {
   app = initializeApp({
     credential: cert(serviceKey as any),
